21962e211ed3f03baeb8f9724e3f3a7aace72cf3
hebabz <heba.bouzidi@etu.unice.fr>
2023-04-18 19:34:30 +0200

    Return and print added

--- runtime-EclipseApplication/NewKotlinModel/lol.kt
+++ runtime-EclipseApplication/NewKotlinModel/lol.kt
@@ -2,13 +2,12 @@ Int0 Int
 
 class Rectangle(width: Int, height: Int) {
     fun area(): Int {
-    	val area : Int;
     	return width * height;
     }
 }
 
 fun main(): Int {
 	val rectangle = Rectangle("10", "20");
-    //"println(rectangle.area())";
+    println("rectangle.area()");
 }
 
